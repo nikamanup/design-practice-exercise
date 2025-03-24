fa573a31c5c7bd88af4754d705fbad7961a271f9
nikamanup <32682231+nikamanup@users.noreply.github.com>
2025-03-24 23:09:35 +0530

    updated example description

--- src/main/java/stream/StreamPractice.java
+++ src/main/java/stream/StreamPractice.java
@@ -30,16 +30,16 @@ public class StreamPractice {
         System.out.println(testString.chars().mapToObj(ch->(char)ch).collect(Collectors.groupingBy(Function.identity(),Collectors.counting())));
  
         oneToTen = List.of( 2, 4,1, 5, 6, 7,3 ,8, 9, 10);
-        System.out.println(oneToTen.stream().sorted((x2,x1)->Integer.compare(x1, x2)).collect(Collectors.toList()));
+        System.out.println("Sorting numbers: "+oneToTen.stream().sorted((x2,x1)->Integer.compare(x1, x2)).collect(Collectors.toList()));
 
         List<String> languageList = List.of("java", "c++", "c", "C sharp", "python", "kotlin", "scala");
         String joinedString=languageList.stream().collect(Collectors.joining(",","[","]"));
-        System.out.println(joinedString);
+        System.out.println("Joining List of element in to single String:"+joinedString);
 
         String joinednumber=oneToTen.stream().map(String::valueOf).collect(Collectors.joining(",","[","]"));
-        System.out.println(joinednumber);
-        System.out.println(oneToTen.stream().max((x1,x2)->Integer.compare(x1,x2)).get());
-        System.out.println(oneToTen.stream().min((x1,x2)->Integer.compare(x1,x2)).get());
+        System.out.println("Joining numbers: "+joinednumber);
+        System.out.println("Max Number from List: "+oneToTen.stream().max((x1,x2)->Integer.compare(x1,x2)).get());
+        System.out.println("Minimum number from List: "+oneToTen.stream().min((x1,x2)->Integer.compare(x1,x2)).get());
 
         int [] randomNumbers ={12, 32, 2, 4, 777, 5, 32, 890, 422, 44, 99, 43};
         int [] randomNumber2 = {4, 3, 2, 5, 6, 78, 98, 53, 90};
@@ -48,10 +48,10 @@ public class StreamPractice {
         .flatMapToInt(Arrays::stream).distinct()  // Flatten the stream of arrays into a single IntStream
         .sorted()  // Sort the stream
         .toArray();
-        System.out.println("Merge"+Arrays.toString(mergedSortedArray));
+        System.out.println("Merge using flatmap:"+Arrays.toString(mergedSortedArray));
       List<Integer> randomNumbers2 = List.of(12, 32, 2, 4, 777, 5, 32, 890, 422, 44, 99, 43);
-      System.out.println(randomNumbers2.stream().sorted(Integer::compare).limit(3).collect(Collectors.toList()));
-      System.out.println(randomNumbers2.stream().sorted((x,y)->Integer.compare(y, x)).limit(3).collect(Collectors.toList()));
+      System.out.println(randomNumbers2.stream().sorted(Integer::compare).limit(3).collect(Collectors.toList()).get(2));
+      System.out.println(randomNumbers2.stream().sorted((x,y)->y.compareTo(x)).collect(Collectors.toList()).get(1));
       int[] sortedArrayByMergingTwoArray=IntStream.concat(Arrays.stream(randomNumbers),Arrays.stream(randomNumber2)).distinct().sorted().toArray();
       System.out.println("merge new"+Arrays.toString(sortedArrayByMergingTwoArray));
       //Anagram
